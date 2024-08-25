2559cf414ca5698ea9b409f2dc322627654854e5
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-08-25 13:30:36 +0200

    Add sample code for matcher

--- SoCraTes2024SourceGen.Playground/Class1.cs
+++ SoCraTes2024SourceGen.Playground/Class1.cs
@@ -5,34 +5,14 @@ namespace SoCraTes2024SourceGen.Playground;
 public enum AwesomeEnum
 {
     A,
-    B,
-    C,
 }
 
 public static class Class1
 {
-    public static int Match(this AwesomeEnum v, Func<int> onA, Func<int> onB, Func<int> onC) => v switch
-    {
-        AwesomeEnum.A => onA(),
-        AwesomeEnum.B => onB(),
-        AwesomeEnum.C => onC(),
-    };
-    
     public static void Problem()
     {
         var value = AwesomeEnum.A;
 
-        var result = value switch
-        {
-            AwesomeEnum.A => 1,
-            AwesomeEnum.B => 2,
-            AwesomeEnum.C => 3,
-        };
-
-        var result2 = value.Match(
-            () => 1,
-            () => 2,
-            () => 3
-            );
+        value.Match(() => 1);
     }
 }
